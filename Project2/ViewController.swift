931a6ae24a00b1633803b1307867af7e8c8029ea
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-03-15 00:37:36 +0700

    initiate

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Runyasak Chaengnaimuang on 15/3/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
